467e14ad6fa15be8c313d4c5a2bb75f9c3be9ec9
Cris <cristobalterueldelacolina@gmail.com>
2026-06-03 17:52:18 -0300

    Cambie logica de estados y agregue variacion entre caminantes

--- caminante.js
+++ caminante.js
@@ -4,16 +4,30 @@ class Caminante {
     this.grosor = random(3, 5);
     this.colorLinea = random(50, 100);
     this.opacidad = random(70, 255);
-    this.variacionAngular = 0;
     this.estado = "curva-circular";  
 
+    //amplitud y frecuencia para variar la forma de la onda
     this.amplitud = 100;
     this.frecuencia = 0.05;
 
+    this.amplitudAlta = random(80, 150);
+    this.frecuenciaAlta = random(0.02, 0.08);
+
+    this.amplitudBaja = random(10, 50);
+    this.frecuenciaBaja = random(0.005, 0.02);
+
+
     this.x = 0;
     this.y = random(height * 0.2, height * 0.8);
-    this.dir = radians(random(360));
     this.vel = 2.5;
+
+    // Variables para curva-circular
+    this.cx = this.x;          // centro del círculo
+    this.cy = this.y;
+    this.radio = random(40, 120);
+    this.angulo = random(TWO_PI);
+    this.velAngular = random(0.02, 0.06) * (random() > 0.5 ? 1 : -1); // sentido aleatorio
+
 
     this.xAnterior = this.x
     this.yAnterior = this.y
@@ -39,30 +53,27 @@ class Caminante {
     //estados 
     if (this.estado === "onda-alta") {
 
-    //  this.variacionAngular = radians(45);
-      this.amplitud = 100;
-      this.frecuencia = 0.04;
+      this.amplitud = this.amplitudAlta;
+      this.frecuencia = this.frecuenciaAlta;
       this.mover();
+      this.cx = this.x;
+      this.cy = this.y;
 
     } else if (this.estado === "onda-baja") {
 
-    //  this.variacionAngular = radians(15);
-      this.amplitud = 30;
-      this.frecuencia = 0.01;
+      this.amplitud = this.amplitudBaja;
+      this.frecuencia = this.frecuenciaBaja;
       this.mover();
+      this.cx = this.x;
+      this.cy = this.y; 
 
     } else if (this.estado === "curva-circular") {
 
-      //  this.variacionAngular = radians(90);
-      // this.amplitud = 50;
-      // this.frecuencia = 0.05;
-      // this.mover();
+      this.angulo += this.velAngular;
 
-      this.dir += 0.08;
-
-      this.x += 2;
-      this.y += 8 * sin(this.dir);
-
+      this.x = this.cx + this.radio * cos(this.angulo);
+      this.y = this.cy + this.radio * sin(this.angulo);
+      this.radio += random(1, 3);   // espiral hacia afuera
     }
 
 
@@ -70,29 +81,8 @@ class Caminante {
   mover() {
     
     this.x += this.vel;
-
     this.y = this.yBase + this.amplitud * sin(this.frecuencia * this.x + this.variacion);
   
-
-    // this.dir += this.frecuencia * this.x + this.variacionAngular
-    //transformación de polares a cartesianas
-    //  this.x = this.vel * cos( this.dir );
-    //  this.y = this.vel * sin( this.dir );
-
-    // this.x = this.x + this.vel;
-    // this.y = this.y + this.dy;
-    
-
-  /*
-    this.x += 0.5;
-    this.y += random(-3, 3);
-
-    this.x += 2;
-    */
-
-    // this.y = this.yBase + map(noise(this.variacionAngular), 0, 1, -150, 150);
-
-    // this.variacionAngular += 0.01;
   }
 
 }
